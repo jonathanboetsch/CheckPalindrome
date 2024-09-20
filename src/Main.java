0d921f3e326b6d7939fed55bf7a33b9bb63c2846
jonathanboetsch <jonathan.boetsch@gmail.com>
2024-09-20 11:21:47 +0200

    Update Main.java
    Create the main program in accordance to the flowchart
    - exception: "Nummer/String" invalid variable name => not needed as one can directly put Scanner input as checkForPalindrome() argument.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        
+        String original = " ";
+        String reverse = " ";
+        while (true) {
+            System.out.println("Ange ett nummer eller string som du anser är en palindrom");
+            Scanner in = new Scanner(System.in);
+            if (!checkForPalindrome(in.nextLine())) {
+                System.out.println("\" \" är inte en palindrom, testa igen");
+            } else {
+                System.out.println(\" \" är en palindrom.");
+                break;
+            }
+        }
     }
 }
