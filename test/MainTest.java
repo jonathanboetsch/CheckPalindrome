570b901afbcf1c0bcc49b5753ded15ff34bb616b
jonathanboetsch <jonathan.boetsch@gmail.com>
2024-09-20 14:02:23 +0200

    Correct assertions in MainTest.java
    Return values in the second and third methods must be true.

--- test/MainTest.java
+++ test/MainTest.java
@@ -3,10 +3,10 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class MainTest {
+public class TestMain {
     @Test
     @DisplayName("Check for validity of the methods' output against words")
-    public static void testCheckForPalindromeAgainstWords() {
+    public void testCheckForPalindromeAgainstWords() {
         assertEquals(true, Main.checkForPalindrome("coloc"));
         assertEquals(true, Main.checkForPalindrome("kayak"));
         assertEquals(true, Main.checkForPalindrome("noon"));
@@ -14,17 +14,17 @@ public class MainTest {
 
     @Test
     @DisplayName("Check for validity of the methods' output against sentences")
-    public static void testCheckForPalindromeAgainstSentences() {
-        assertEquals(false, Main.checkForPalindrome("Never odd or even"));
-        assertEquals(false, Main.checkForPalindrome("Don’t nod"));
-        assertEquals(false, Main.checkForPalindrome("A man, a plan, a canal, Panama!"));
-        assertEquals(false, Main.checkForPalindrome("Step on no pets!"));
+    public void testCheckForPalindromeAgainstSentences() {
+        assertEquals(true, Main.checkForPalindrome("Never odd or even"));
+        assertEquals(true, Main.checkForPalindrome("Don’t nod"));
+        assertEquals(true, Main.checkForPalindrome("A man, a plan, a canal, Panama!"));
+        assertEquals(true, Main.checkForPalindrome("Step on no pets!"));
     }
     @Test
     @DisplayName("Check for validity of the methods' output against numbers")
-    public static void testCheckForPalindromeAgainstNumbers() {
-        assertEquals(false, Main.checkForPalindrome("888"));
-        assertEquals(false, Main.checkForPalindrome("1,234,321"));
-        assertEquals(false, Main.checkForPalindrome("2/2/22"));
+    public void testCheckForPalindromeAgainstNumbers() {
+        assertEquals(true, Main.checkForPalindrome("888"));
+        assertEquals(true, Main.checkForPalindrome("1,234,321"));
+        assertEquals(true, Main.checkForPalindrome("2/2/22"));
     }
 }
