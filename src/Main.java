1d48674408d2180ad5b5e7bf17762f48754f957f
123Riho <ronja.olvall@iths.se>
2024-09-20 14:43:05 +0200

    Update Main.java Upptadering av metoderna

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,9 @@ public class Main {
             System.out.println("Ange ett nummer eller string som du anser är en palindrom");
             Scanner in = new Scanner(System.in);
             String input = in.nextLine();
-            if (!checkForPalindrome(input)) {
+            String reverse = toReverse(input);
+            
+            if (!checkForPalindrome(input, reverse)) {
                 System.out.println(input + " är inte en palindrom, testa igen");
             } else {
                 System.out.println(input + " är en palindrom.");
@@ -15,7 +17,7 @@ public class Main {
         }
     }
 
-    public static boolean checkForPalindrome(String originalWordOrNumber){
+    public static String toReverse(String originalWordOrNumber){
         originalWordOrNumber.toCharArray();
         char letter;
         String reverse = "";
@@ -23,26 +25,14 @@ public class Main {
             letter = originalWordOrNumber.charAt(i);
             reverse = letter + reverse;
         }
+        return reverse;
+    }
 
+    public static boolean checkForPalindrome(String originalWordOrNumber, String reverse){
         reverse = reverse.replaceAll("\\s", "");
 
         if (reverse.equals(originalWordOrNumber.replaceAll("\\s", ""))){
             return true;
-        }
-        return false;
-    }
-
-    public static boolean checkForPalindrome2(String originalWordOrNumber){
-        for(int i=0; i<originalWordOrNumber.length()-1; i++){
-            if (originalWordOrNumber.charAt(i) == originalWordOrNumber.charAt(originalWordOrNumber.length()-i-1)){
-                if (i == originalWordOrNumber.length()-i-1){
-                    return true;
-                } else if (i == originalWordOrNumber.length()-i){
-                    return true;
-                }
-            } else {
-                return false;
-            }
         }
         return false;
     }
