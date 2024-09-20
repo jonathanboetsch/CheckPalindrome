4bef97ce94a4ceec8418a0fdf08121a287df055b
jonathanboetsch <jonathan.boetsch@gmail.com>
2024-09-20 15:19:00 +0200

    Add test against toReverse() in MainTest.java

--- test/MainTest.java
+++ test/MainTest.java
@@ -2,29 +2,46 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class TestMain {
     @Test
     @DisplayName("Check for validity of the methods' output against words")
     public void testCheckForPalindromeAgainstWords() {
-        assertEquals(true, Main.checkForPalindrome("coloc"));
-        assertEquals(true, Main.checkForPalindrome("kayak"));
-        assertEquals(true, Main.checkForPalindrome("noon"));
+        assertTrue(Main.checkForPalindrome("coloc"));
+        assertTrue(Main.checkForPalindrome("kayak"));
+        assertTrue(Main.checkForPalindrome("noon"));
     }
 
     @Test
-    @DisplayName("Check for validity of the methods' output against sentences")
+    @DisplayName("Test checkForPalindrome() against sentences")
     public void testCheckForPalindromeAgainstSentences() {
-        assertEquals(true, Main.checkForPalindrome("Never odd or even"));
-        assertEquals(true, Main.checkForPalindrome("Don’t nod"));
-        assertEquals(true, Main.checkForPalindrome("A man, a plan, a canal, Panama!"));
-        assertEquals(true, Main.checkForPalindrome("Step on no pets!"));
+        assertTrue(Main.checkForPalindrome("Never odd or even"));
+        assertTrue(Main.checkForPalindrome("Don’t nod"));
+        assertTrue(Main.checkForPalindrome("A man, a plan, a canal, Panama!"));
+        assertTrue(Main.checkForPalindrome("Step on no pets!"));
     }
+
     @Test
-    @DisplayName("Check for validity of the methods' output against numbers")
+    @DisplayName("Test checkForPalindrome() against numbers")
     public void testCheckForPalindromeAgainstNumbers() {
-        assertEquals(true, Main.checkForPalindrome("888"));
-        assertEquals(true, Main.checkForPalindrome("1,234,321"));
-        assertEquals(true, Main.checkForPalindrome("2/2/22"));
+        assertTrue(Main.checkForPalindrome("888"));
+        assertTrue(Main.checkForPalindrome("1,234,321"));
+    }
+
+    @Test
+    @DisplayName("Check cleanText() output")
+    public void testCleanText() {
+        assertEquals("neveroddoreven", Main.formatText("Never odd or even"));
+        assertEquals("dontnod", Main.formatText("Don’t nod"));
+        assertEquals("amanaplanacanalpanama", Main.formatText("A man, a plan, a canal, Panama!"));
+    }
+
+    @Test
+    @DisplayName("Check for validity of toReverse method")
+    public void testToReverseMethod() {
+        // assertEquals("emordnilap", Main.toReverse("palindrome"));
+        assertEquals("rakso", Main.toReverse("oskar"));
+        assertEquals("nahtanoJ", Main.toReverse("Jonathan"));
     }
 }
